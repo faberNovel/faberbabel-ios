63c1c27f3ef34d200881bbf00ea1d2bff844f034
Jean Haberer <jean.haberer@fabernovel.com>
2020-07-10 13:59:18 +0200

    Update Translation

--- Faberbabel/Classes/String+Translation.swift
+++ Faberbabel/Classes/String+Translation.swift
@@ -13,19 +13,23 @@ extension String {
     }
 
     public func fb_translate(to lang: String) -> String {
+        var localized = fb_localize(to: lang)
+        if localized == self {
+            EventNotifier.shared?.notify(events: [Event(type: .missing_key, key: self)])
+            if lang != "en" {
+                localized = fb_localize(to: "en")
+            }
+        }
+        return localized
+    }
+
+    func fb_localize(to lang: String) -> String {
         guard
             let directoryUrl = Bundle.updatedLocalizablesBundle?.localizableDirectoryUrl?.appendingPathComponent("\(lang).lproj"),
             let bundle = Bundle(path: directoryUrl.path)
             else {
                 return NSLocalizedString(self, comment: "")
-            }
-        var localized = bundle.localizedString(forKey: self, value: nil, table: nil)
-        if localized == self {
-            EventNotifier.shared?.notify(events: [Event(type: .missing_key, key: self)])
-            if lang != "en" {
-                localized = fb_translate(to: "en")
-            }
         }
-        return localized
+        return bundle.localizedString(forKey: self, value: nil, table: nil)
     }
 }
