42c913d2fe2e03cc06184a4411378eb6f17ea27a
Jean Haberer <jean.haberer@fabernovel.com>
2020-07-15 12:09:13 +0200

    Clean Widget File

--- Example/Widget/TodayViewController.swift
+++ Example/Widget/TodayViewController.swift
@@ -18,22 +18,11 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     override func viewDidLoad() {
         super.viewDidLoad()
         Bundle.fb_addAppGroupIdentifier("group.faberbabel.com")
-        // Do any additional setup after loading the view.
     }
 
     @IBAction private func localize() {
         label.text = "hello_world_title".fb_translation
         button.setTitle("localize_button".fb_translation, for: .normal)
-    }
-
-    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-
-        completionHandler(NCUpdateResult.newData)
     }
 
 }
